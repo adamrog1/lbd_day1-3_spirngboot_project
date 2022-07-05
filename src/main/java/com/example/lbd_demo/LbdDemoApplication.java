f572493bb4c1a14eb3ab00f4004e68d451997639
Adam Rogalski <adamcio.rogalski1@gmail.com>
2022-07-05 10:32:41 +0200

    first_commit

---
+++ src/main/java/com/example/lbd_demo/LbdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.lbd_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbdDemoApplication.class, args);
+    }
+
+}
